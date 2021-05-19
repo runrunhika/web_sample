06704080eda48c741c4eca952ba4c72cb61ba857
runrunhika <runrun09in@gmail.com>
2021-05-19 15:30:31 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Image.asset("logo.png"),
+          Image.network(
+            'https://tech.pepabo.com/blog/2020/05/27/flutter-hands-on/eye_catch.jpg',
+            fit: BoxFit.cover,
+          )
+        ],
+      ),
+    );
+  }
+}
