34f5d5dc3df8af18c433f9e65fe28be477227383
runrunhika <runrun09in@gmail.com>
2021-05-26 09:30:12 +0900

    second deploy

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   runApp(MyApp());
 }
 
@@ -26,13 +26,24 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: Column(
         children: [
-          Image.asset("logo.png"),
+          Image.asset('assets/download.jpg'),
           Image.network(
-            'https://tech.pepabo.com/blog/2020/05/27/flutter-hands-on/eye_catch.jpg',
+            'https://firebasestorage.googleapis.com/v0/b/flutter-web-82849.appspot.com/o/siteImg%2Ffire.png?alt=media&token=b93ee45c-1a4f-467a-a06e-cfdef062a956',
             fit: BoxFit.cover,
-          )
+          ),
+          Image.network(
+            'https://github.com/runrunhika/photos/blob/main/firebase.png?raw=true',
+            fit: BoxFit.contain,
+          ),
+          Image.network(
+            'https://github.com/runrunhika/photos/blob/main/flutter.png?raw=true',
+            fit: BoxFit.fitHeight,
+          ),
+          Image.network(
+              'https://github.com/runrunhika/photos/blob/main/hart.jpg?raw=true')
         ],
       ),
     );
