0e9d652e625468f509032aa51b3260b368506559
runrunhika <runrun09in@gmail.com>
2021-06-07 20:19:34 +0900

    first deploy

--- lib/main.dart
+++ lib/main.dart
@@ -40,106 +40,127 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "1  スタンプで会話する",
-              style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Text(
-                "間抜けなので、言語能力が著しく欠損してるのでしょうね",
-                style:
-                    TextStyle(fontSize: 20, color: Colors.grey.withOpacity(.7)),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage("assets/unnamed.jpg"),
+            fit: BoxFit.cover,
+          )),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "1  スタンプで会話する",
+                style: TextStyle(
+                    fontSize: 60,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
               ),
-            ),
-            Divider(),
-            Text(
-              "2  花火やりたがる",
-              style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Text(
-                "光があるところに集まるようです。虫のようですね",
-                style:
-                    TextStyle(fontSize: 20, color: Colors.grey.withOpacity(.7)),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                  "間抜けなので、言語能力が著しく欠損してるのでしょうね",
+                  style: TextStyle(
+                      fontSize: 20, color: Colors.white.withOpacity(.7)),
+                ),
               ),
-            ),
-            Divider(),
-            Text(
-              "3  白石町在住",
-              style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Text(
-                "治安が悪い地域のようです。これ以上の詮索は良しとしましょう",
-                style:
-                    TextStyle(fontSize: 20, color: Colors.grey.withOpacity(.7)),
+              Divider(),
+              Text(
+                "2  花火やりたがる",
+                style: TextStyle(
+                    fontSize: 60,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
               ),
-            ),
-            Divider(),
-            Text(
-              "4  井上　友一　ってやつ",
-              style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Text(
-                "顔相占い的にみて、雨　のち　地獄でしょう　\n\n\n てか、きっしょ　うえぇえぇぇ",
-                style:
-                    TextStyle(fontSize: 20, color: Colors.grey.withOpacity(.7)),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                  "光があるところに集まるようです。虫のようですね",
+                  style: TextStyle(
+                      fontSize: 20, color: Colors.white.withOpacity(.7)),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 100,
-            ),
-            Container(
-              width: double.infinity,
-              height: 70,
-              decoration: BoxDecoration(border: Border.all(color: Colors.pink)),
-              child: Align(
-                  alignment: Alignment.center,
-                  child: Text(
-                    "他の記事をみてみる",
-                    style: TextStyle(fontSize: 40),
+              Divider(),
+              Text(
+                "3  白石町在住",
+                style: TextStyle(
+                    fontSize: 60,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                  "治安が悪い地域のようです。これ以上の詮索は良しとしましょう",
+                  style: TextStyle(
+                      fontSize: 20, color: Colors.white.withOpacity(.7)),
+                ),
+              ),
+              Divider(),
+              Text(
+                "4  井上　友一　ってやつ",
+                style: TextStyle(
+                    fontSize: 60,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                  "顔相占い的にみて、雨　のち　地獄でしょう　\n\n\n てか、きっしょ　うえぇえぇぇ",
+                  style: TextStyle(
+                      fontSize: 20, color: Colors.white.withOpacity(.7)),
+                ),
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              Container(
+                width: double.infinity,
+                height: 70,
+                decoration:
+                    BoxDecoration(border: Border.all(color: Colors.pink)),
+                child: Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "他の記事をみてみる",
+                      style: TextStyle(fontSize: 40, color: Colors.white),
+                    )),
+              ),
+              Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (ctx) => Page1()));
+                    },
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text('Mさんは語る！　井上の個人情報！！！',
+                          style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              color: Colors.white)),
+                    ),
                   )),
-            ),
-            Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context, MaterialPageRoute(builder: (ctx) => Page1()));
-                  },
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Text('Mさんは語る！　井上の個人情報！！！',
-                        style: TextStyle(
-                          decoration: TextDecoration.underline,
-                        )),
-                  ),
-                )),
-                Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context, MaterialPageRoute(builder: (ctx) => Page2()));
-                  },
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Text('秘密の部屋',
-                        style: TextStyle(
-                          decoration: TextDecoration.underline,
-                        )),
-                  ),
-                )),
-          ],
+              Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (ctx) => Page2()));
+                    },
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text('秘密の部屋',
+                          style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              color: Colors.white)),
+                    ),
+                  )),
+            ],
+          ),
         ),
       ),
     );
